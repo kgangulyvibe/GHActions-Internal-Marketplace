49b2e6eb04b5723e2d84ad1123be7dc2f3c81509
K G <3813135+kcodeg123@users.noreply.github.com>
2024-03-27 11:20:36 +0100

    The most significant changes in the code are primarily focused on improving readability and clarity. This is achieved through renaming variables, adding detailed comments, and modifying the output of functions.
    In the `issue-labeled-as-security-scan.yml` file, output variables have been renamed for clarity, such as `name` to `actionName` and `owner` to `actionOwner`. Additional comments have also been added to explain the purpose of each output variable.
    
    In the `find-action-from-issue.js` file, comments have been added to explain the purpose of each line of code. Variables have been renamed for clarity, such as `action` to `actionOwnerNameVersion`. The action name and version, which were previously combined, have now been separated into individual variables. The output of the function has been modified to include the new variables `actionOwnerNameVersion`, `actionOwner`, and `actionName`.
    
    Changes:
    
    1. Renaming output variables in `issue-labeled-as-security-scan.yml` for clarity.
    2. Adding comments to output variables in `issue-labeled-as-security-scan.yml`.
    3. Adding comments to explain the purpose of each line of code in `find-action-from-issue.js`.
    4. Renaming variables in `find-action-from-issue.js` for clarity.
    5. Separating the action name and version into individual variables in `find-action-from-issue.js`.
    6. Modifying the output of the function in `find-action-from-issue.js` to include new variables.

--- src/find-action-from-issue.js
+++ src/find-action-from-issue.js
@@ -2,12 +2,12 @@
 // It will return the action name and owner
 // It will also set the output variables for the next steps to use
 
-// Inputs
+// Inputs are provided by the workflow file when calling the action.
 // - github: the github object
-// - owner: the owner of the repository
-// - repo: the repository name
+// - owner: the owner of the repository where the issue is located
+// - repo: the repository name where the issue is located
 // - issue_number: the issue number to look at
-// - core: the core object
+// - core: the core object which is used to set the output variables
 
 // Outputs
 // - result: 0 if action found, 1 if not found
@@ -15,13 +15,15 @@
 // - owner: the owner of the action
 // - name: the name of the action
 
-module.exports = async ({github, owner, repo, issue_number, core}) => {
-
+module.exports = async ({github, owner, repo, issue_number, core}) => { // we use async to make sure we can use await in the function body. The arguments are the inputs we need to run the action which are sent by the workflow file when calling the action
+    // module.exports is used to export the function so it can be used in the workflow file when calling the action with the inputs
+    // The function is async so we can use await in the function body
+    // The function takes the inputs we need to run the action as arguments
   console.log(`Looking at this repository: [${owner}/${repo}]`)
   console.log(`Running with issue number [${issue_number}]`)
 
   // we always need these in the next steps:
-  console.log(`::set-output name=request_owner::${owner}`)            
+  console.log(`::set-output name=request_owner::${owner}`)   
   console.log(`::set-output name=request_repo::${repo}`)
   console.log(`::set-output name=request_issue::${issue_number}`)
 
@@ -34,16 +36,16 @@ module.exports = async ({github, owner, repo, issue_number, core}) => {
     owner: owner,
     repo: repo,
     issue_number: issue_number,
-  })
+  }) // github.rest.issues.get is used to get the issue body from the repository. We use await to make sure we wait for the response before continuing
   
   console.log(`Issue body: [${JSON.stringify(issue.data.body)}]`)
 
   let split
   if (issue.data.body.indexOf('\r\n') > -1) {
-    split = issue.data.body.split(/\r\n/) // normal issue
+    split = issue.data.body.split(/\r\n/) // issue.data.body.split(/\r\n/) is used to split the body of the issue by new lines. We use \r\n because it is the new line character in Windows
   }
   else {
-    split = issue.data.body.split(/\n\n/) // issue from template
+    split = issue.data.body.split(/\n\n/) // issue.data.body.split(/\n\n/) is used to split the body of the issue by new lines. We use \n\n because it is the new line character in Unix
   }
 
   let action
@@ -53,11 +55,11 @@ module.exports = async ({github, owner, repo, issue_number, core}) => {
     if (split[i].startsWith('uses: ')) {
       console.log(`Found uses statement!`)
         
-      action = split[i].substring(6)
-      let spaceIndex = action.indexOf(' ')
+      action = split[i].substring(6) // as an example "uses: actions/checkout@v2". we use substring(6) to get "actions/checkout@v2" because we know the length of "uses: " is 6
+      let spaceIndex = action.indexOf(' ') // if there is a space, we need to cut off the action name, as an example "uses: actions/checkout@v2 with: {ref: 'refs/heads/feature-branch'}". we use indexOf(' ') to get the index of the first space and then use substring(0, spaceIndex) to get "actions/checkout@v2"
       if (spaceIndex > 0) {
         console.log(`Found space at char [${spaceIndex}], cutting of the action text before it`)
-        action = action.substring(0, spaceIndex)        
+        action = action.substring(0, spaceIndex)       
       }
       console.log(`Found action with name [${action}]`)
       break
@@ -77,17 +79,20 @@ module.exports = async ({github, owner, repo, issue_number, core}) => {
 
   // return action
   if (result === 0) {
-    let index = action.indexOf('/')
+    let index = action.indexOf('/') // We do action.indexOf('/') to find the first slash in the action name. This is to split the owner and the action name. For example, index = 7 for "actions/checkout@v2" and we use substring(0, 7) to get "actions" and substring(8) to get "checkout@v2"
     let actionOwner = action.substring(0, index)
-    let actionName = action.substring(index+1)
+    let actionNameWithVersion = action.substring(index + 1)
+
+    // define the action without the version
+    let actionName = actionNameWithVersion.split('@')[0]
 
     console.log(`Found owner:${actionOwner}`)
     console.log(`Found action:${actionName}`)
 
-    console.log(`::set-output name=action::${action}`)
-    console.log(`::set-output name=owner::${actionOwner}`)
-    console.log(`::set-output name=name::${actionName}`)
+    console.log(`::set-output name=actionOwnerNameVersion::${action}`) // example of this output is actionRepo = "actions/checkout@v2"
+    console.log(`::set-output name=actionOwner::${actionOwner}`) // example of this output is actionOwner = "actions"
+    console.log(`::set-output name=actionName::${actionName}`) // example of this output is actionName = "checkout"
   }
 
-  return { result, action }
+  return { result, action } // example output of result: 0, action: "actions/checkout@v2"
 }
